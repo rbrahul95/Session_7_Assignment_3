347ce1bde836a5d122a2f79d323e242e38ff430b
rbrahul95 <rbrahul95@gmail.com>
2016-09-18 21:58:07 +0530

    Add files via upload

---
+++ Session_7_Assignment_3.java
@@ -0,0 +1,34 @@
+/*Assignment – Create student result processing system with an abstract
+class.
+*/
+
+public class Session_7_Assignment_3 {
+    
+	public static void main(String[] args) {
+		
+			
+		try{
+			String a = null;
+			if(a.equals("xyz"))
+				System.out.println(a);
+			
+		}
+		catch(Exception e){
+			
+			System.out.println(e);
+			
+		}
+
+	}
+
+}
+
+/* 
+  Output :   
+ 
+
+   java.lang.NullPointerException
+  
+ 
+*/
+
